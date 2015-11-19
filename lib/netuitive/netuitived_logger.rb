1185b3cf12ff550eb87d7b476a972c9a7dcd2249
John King <john.king@netuitive.com>
2015-11-19 07:12:23 -0500

    adding a hack to make the logging fault tolerant if we don't have permission to the log file

--- lib/netuitive/netuitived_logger.rb
+++ lib/netuitive/netuitived_logger.rb
@@ -1,6 +1,21 @@
 require 'logger'
+class CheaterLogger
+	attr_accessor :level
+	def debug(message)
+	end
+	def error(message)
+	end
+	def info(message)
+	end
+end
+
 class NetuitiveLogger
-	@@log = Logger.new("#{File.expand_path("../../..", __FILE__)}/log/netuitive.log",'daily', 10)
+	begin
+		@@log = Logger.new("#{File.expand_path("../../..", __FILE__)}/log/netuitive.log",'daily', 10)
+	rescue
+		puts "netuitive unable to open log file"
+		@@log = CheaterLogger.new
+	end
 	class << self
 		def log
 			return @@log
