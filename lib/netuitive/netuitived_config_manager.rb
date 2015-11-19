bae184348838cf11e2c11bf95eb0d2d3e37b179f
John King <john.king@netuitive.com>
2015-11-19 06:39:07 -0500

    adding configuration using environment variables

--- lib/netuitive/netuitived_config_manager.rb
+++ lib/netuitive/netuitived_config_manager.rb
@@ -38,16 +38,40 @@ class ConfigManager
 		def readConfig()
 			gem_root= File.expand_path("../../..", __FILE__)
 			data=YAML.load_file "#{gem_root}/config/agent.yml"
-			@@apiId=data["apiId"]
-			@@baseAddr=data["baseAddr"]
-			@@port=data["port"]
-			@@elementName=data["elementName"]
-			@@netuitivedAddr=data["netuitivedAddr"]
-			@@netuitivedPort=data["netuitivedPort"]
-			@@interval=data["interval"]
+			@@apiId=ENV["NETUITIVED_API_ID"]
+			if(@@apiId == nil or @@apiId == "")
+				@@apiId=data["apiId"]
+			end
+			@@baseAddr=ENV["NETUITIVED_BASE_ADDR"]
+			if(@@baseAddr == nil or @@baseAddr == "")
+				@@baseAddr=data["baseAddr"]
+			end
+			@@port=ENV["NETUITIVED_PORT"]
+			if(@@port == nil or @@port == "")
+				@@port=data["port"]
+			end
+			@@elementName=ENV["NETUITIVED_ELEMENT_NAME"]
+			if(@@elementName == nil or @@elementName == "")
+				@@elementName=data["elementName"]
+			end
+			@@netuitivedAddr=ENV["NETUITIVED_NETUITIVED_ADDR"]
+			if(@@netuitivedAddr == nil or @@netuitivedAddr == "")
+				@@netuitivedAddr=data["netuitivedAddr"]
+			end
+			@@netuitivedPort=ENV["NETUITIVED_NETUITIVED_PORT"]
+			if(@@netuitivedPort == nil or @@netuitivedPort == "")
+				@@netuitivedPort=data["netuitivedPort"]
+			end
+			@@interval=ENV["NETUITIVED_INTERVAL"]
+			if(@@interval == nil or @@interval == "")
+				@@interval=data["interval"]
+			end
+			debugLevelString=ENV["NETUITIVED_DEBUG_LEVEL"]
+			if(debugLevelString == nil or debugLevelString == "")
+				debugLevelString=data["debugLevel"]
+			end
 			NetuitiveLogger.log.info "port: #{@@netuitivedPort}"
 			NetuitiveLogger.log.info "addr: #{@@netuitivedAddr}"
-			debugLevelString=data["debugLevel"]
 			if debugLevelString == "error"
 				NetuitiveLogger.log.level = Logger::ERROR
 			elsif debugLevelString == "info"
