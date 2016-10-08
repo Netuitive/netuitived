f71e7eded1671c6718c372842a224c3f7ca8aa10
Keith Gable <keith@art19.com>
2016-10-07 19:15:35 -0700

    Rework how Netuitived is started via bin/netuitived so that it can be started in the foreground and so netuitived isn't dependent on global objects

--- lib/netuitive/scheduler.rb
+++ lib/netuitive/scheduler.rb
@@ -1,14 +1,15 @@
 require 'netuitive/netuitived_config_manager'
 require 'drb/drb'
+
 class Scheduler
 	def self.startSchedule
 		Thread.new do
   			while true do
   				sleep(ConfigManager.interval)
   				Thread.new do
-    				FRONT_OBJECT.sendMetrics
+    				Netuitived.front_object.sendMetrics
     			end
   			end
 		end
 	end
-end 
+end
